664f0175c758bd9541304f3a3b20a6efceb180a4
Dipto K Kundu <dkkundu00@gmail.com>
2021-07-22 15:08:02 +0600

    Add change record options

--- src/bloodlagbe/DashboardController.java
+++ src/bloodlagbe/DashboardController.java
@@ -5,12 +5,22 @@
  */
 package bloodlagbe;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 /**
@@ -21,8 +31,14 @@ public class DashboardController implements Initializable {
     
 
     @FXML
-    void btnAddRecord(ActionEvent event) {
+    void btnAddRecord(ActionEvent event) throws IOException {
         System.out.println("Clicked BTN ADD RECORD");
+        Parent root = FXMLLoader.load(getClass().getResource("/addRecord/addRecord.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage(StageStyle.DECORATED);
+        stage.setTitle("Add Record");
+        stage.setScene(scene);
+        stage.show();
 
     }
 
